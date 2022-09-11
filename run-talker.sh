1e8b583422c283969e997ebf46d2c931634667f4
weixian.zhou <weixian.zhou@ninebot.com>
2022-09-11 11:13:49 +0000

    remove generated code

--- run-talker.sh
+++ run-talker.sh
@@ -7,4 +7,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`rospack find rosjava_jni`/jni/build
 cd src
 
 LIB_DIR=/root/rosnodejs_ws/src/hello_java/build/libs
-java -classpath $LIB_DIR/hello_java-1.0.1.jar:$LIB_DIR/rosjni-1.0.0.jar Talker
+java -classpath $LIB_DIR/hello_java-1.0.1.jar:$LIB_DIR/rosjni-linux-aarch64-1.0.0.jar Talker
--- scripts/build-deps.sh
+++ scripts/build-deps.sh
@@ -28,13 +28,17 @@ for l in `cat depend`;do
 	k=`echo $l | cut -d/ -f1`
 	v=`echo $l | cut -d/ -f2`
 	t=`echo $v | cut -d. -f2`
+	echo "$k $v $t"
 	pkg_dir=`rospack find $k`
-	[[ ! -e $pkg_dir ]] && log "rospack $k not found", skip ; continue
-	if [[ $t -eq 'msg' ]];then
-		[[ ! -e $pkg_dir/msg/$v ]] && continue
+	echo $pkg_dir
+	[[ ! -e $pkg_dir ]] && log "rospack $k not found" 
+	if [[ $t == 'msg' ]];then
+		[[ ! -e $pkg_dir/msg/$v ]] && continue || echo $pkg_dir/msg/$v
+		echo ./scripts/genmsg_java.py $pkg_dir/msg/$v ./src
 		./scripts/genmsg_java.py $pkg_dir/msg/$v ./src
-	elif [[ $t -eq 'srv' ]]; then
-		[[ ! -e $pkg_dir/srv/$v ]] && continue
+	elif [[ $t == 'srv' ]]; then
+		[[ ! -e $pkg_dir/srv/$v ]] && continue || echo $pkg_dir/srv/$v
+		echo ./scripts/gensrv_java.py $pkg_dir/srv/$v ./src
 		./scripts/gensrv_java.py $pkg_dir/srv/$v ./src
 	else
 		die "Unknown format"
